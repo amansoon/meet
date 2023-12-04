77b856435d0f09bcbe55b3dbd599b37a410d6e53
Aman Ghanghoriya <121393968+amansoon@users.noreply.github.com>
2023-12-04 12:52:54 +0000

    call and answer setup

--- client/app/connect/Call.tsx
+++ client/app/connect/Call.tsx
@@ -1,25 +1,37 @@
 'use client'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import { Peer } from "peerjs";
 import { v4 as uuid } from 'uuid';
 import axios from 'axios'
 
-const baseUrl = "https://turbo-tribble-44v5rxggpvqf5jjx-3000.app.github.dev/api/peer"
+const baseUrl = "http://localhost:8000"
 
 async function createPeer(peerId : string) {
-    const res = await fetch(baseUrl, {
-        method: 'POST',
-        mode: 'no-cors',
-        body: JSON.stringify({peerId: 'aman' }),
-    });
+    const res = await axios.post(`${baseUrl}/api/peer`, {
+        peerId : peerId
+    })
+    console.log("response = ", res)
 }
 
 export default function Call() {
     const [peer, setPeer] = useState();
-    const [anotherPeerId, setAnotherPeerId] = useState();
+    const [peerId, setPeerId] = useState();
+    const videoRef = useRef<HTMLVideoElement>();
 
     useEffect(() => {
-        setPeer(new Peer(uuid()));
+        const peer = new Peer(uuid());
+
+        peer.on('open', (id) => {
+           console.log('My peer ID is: ', id);
+           createPeer(id);
+        });
+
+        peer.on('error', (error) => {
+           console.log(error);
+        });
+
+        // store peer to server
+        setPeer(peer);
     }, []);
 
     useEffect(() => {
@@ -27,11 +39,9 @@ export default function Call() {
             return;
         }
 
-        // store peer to server
-        createPeer(peer.id);
-
-        // receive
+        // Handle incoming data connection
         peer.on("connection", (conn) => {
+            console.log("Incoming peer connection!")
             conn.on("data", (data) => {
                 console.log("data = ", data);
             });
@@ -39,21 +49,69 @@ export default function Call() {
                 conn.send("hello!");
             });
         });
+
+
+        // Handle incoming voice/video connection
+        peer.on('call', (call) => {
+            navigator.mediaDevices.getUserMedia({video: true, audio: true})
+            .then((stream) => {
+                call.answer(stream); // Answer the call with an A/V stream.
+                call.on('stream', renderVideo);
+            })
+            .catch((err) => {
+                console.error('Failed to get local stream', err);
+            });
+        });   
+        
     }, [peer])
 
     useEffect(() => {
-        if(anotherPeerId) {
-            connectToPeer(anotherPeerId)
+        if(peerId) {
+            connectToPeer()
         }
-    }, [anotherPeerId]);
+    }, [peerId]);
 
 
-    const connectToPeer = (peerId) => {
-        const conn = peer.connect(peerId);
-        conn.on("open", () => {
-            conn.send("hi!");
+    const connectToPeer = () => {
+        console.log(`Connecting to ${peerId}...`);
+        
+        let conn = peer.connect(peerId);
+        conn.on('data', (data) => {
+          console.log(`received: ${data}`);
         });
+        conn.on('open', () => {
+          conn.send('hi!');
+        });
+        
+        navigator.mediaDevices.getUserMedia({video: true, audio: true})
+          .then((stream) => {
+            let call = peer.call(peerId, stream);
+            call.on('stream', renderVideo);
+          })
+          .catch((err) => {
+            console.log('Failed to get local stream', err);
+          });
     }
+
+    const connectHandler = async () => {
+        try {
+            const res = await axios.get(`${baseUrl}/api/peer/${peer.id}`)
+            const data = res.data.data;
+            console.log(data)
+            if(data.anotherPeerId) {
+                setPeerId(data.anotherPeerId);
+            }
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
+
+    const renderVideo = (stream) => {
+        if(videoRef.current) {
+            videoRef.current.srcObject = stream;
+        }
+    };
 
     return (
         <div>
@@ -62,8 +120,15 @@ export default function Call() {
             {peer && (
               <div> Peer ID = {peer.id} </div>
             )}
-            <button className="px-4 py-3 rounded bg-slate-200"> connect to stranger </button>
+            <button onClick={connectHandler} className="px-4 py-3 rounded bg-slate-200"> connect to stranger </button>
           </div>
+
+          <br />
+
+          <div className="p-4">
+            <video ref={videoRef} width="320" height="240" controls></video>
+          </div>
+
         </div>
     )
 }
